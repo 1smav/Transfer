b05b8a9f73d41245bb79d1a80d9d4687e1b91d0a
1smav <vamsi.kasturi@gmail.com>
2017-01-15 22:00:45 +0100

    Add project files.

---
+++ ICD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ICD.MvcApplication" Language="C#" %>
---
+++ ICD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ICD.Startup))]
+namespace ICD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
